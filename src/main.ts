44ddd3071d5eca1edeb35d3d0e92dcb33d59a917
Linus Juhlin <linus.juhlin@protonmail.com>
2022-07-26 07:00:24 +0900

    ZEN-9784 Initial commit

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function run(): Promise<void> {
+  try {
+    const token = core.getInput('token');
+    // API: https://actions-cool.github.io/octokit-rest/
+    const octokit = github.getOctokit(token);
+
+    const context = github.context;
+
+    const { owner, repo } = context.repo;
+    const prNumber = context.payload.pull_request?.number ?? -1;
+
+    if (prNumber === -1) {
+      throw new Error('Invalid PR number');
+    }
+
+    const resp = await octokit.rest.pulls.listFiles({
+      owner: owner,
+      repo: repo,
+      pull_number: prNumber,
+    });
+
+    const validFiles = resp.data.filter((file) => file.filename.endsWith('.php'));
+
+    core.setOutput('has_files', validFiles.length > 0);
+    core.setOutput('files', validFiles.join(' '));
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
